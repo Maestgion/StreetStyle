9ac83d19624e66a0f8daf36657c4fd15c13f3f1d
abhishek <7abhishek1410@gmail.com>
2024-04-20 23:03:16 +0530

    product-schema-completed

--- server/src/models/product.model.js
+++ server/src/models/product.model.js
@@ -1,7 +1,52 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+    {
+        category: {
+          ref: "Category",
+          required: true,
+          type: Schema.Types.ObjectId,
+        },
+        description: {
+          required: true,
+          type: String,
+        },
+        mainImage: {
+          required: true,
+          type: {
+            url: String,
+            localPath: String,
+          },
+        },
+        name: {
+          required: true,
+          type: String,
+        },
+        owner: {
+          ref: "User",
+          type: Schema.Types.ObjectId,
+        },
+        price: {
+          default: 0,
+          type: Number,
+        },
+        stock: {
+          default: 0,
+          type: Number,
+        },
+        subImages: {
+          type: [
+            {
+              url: String,
+              localPath: String,
+            },
+          ],
+          default: [],
+        },
+      },
+      { timestamps: true }
+    );
    
-})
+
 
 export const Product = mongoose.model("products", productSchema)
