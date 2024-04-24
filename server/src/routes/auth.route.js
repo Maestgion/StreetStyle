18852427d8f79b14337c94aab5e1ec4fa93bef56
abhishek <7abhishek1410@gmail.com>
2024-04-24 23:29:09 +0530

    user-routes-dones

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,7 +1,45 @@
-// register, login, get user details,  update, reset-password, forgot-password, logout
+import {Router} from 'express'
+import {
+    registerUser,
+    loginUser,
+    verifyEmail,
+    resendEmailVerification,
+    forgotPassword,
+    resetForgottenPassword,
+    changeCurrentPassword,
+    assignRole,
+    refreshAccessToken,
+    getCurrentUser,
+    logoutUser
+} from "../controllers/auth.controller"
+import {
+    verifyJWT,
+verifyResourcePermission
+} from "../middlewares/auth.middleware"
+import { UserRolesEnum } from '../constants'
 
-import Router from 'express'
 
-const router = Router();
+const router = Router()
 
-router.post('/register')
+router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
+
+
+router.route("/verify-email/:verificationToken").post(verifyEmail)
+router.route("/resend-email-verification").post(verifyJWT, resendEmailVerification)
+
+
+router.route("/forgot-password").post(forgotPassword)
+router.route("/reset-password/:resetToken").post(resetForgottenPassword)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+
+router.route("/assign-role/:userId").post(verifyJWT, verifyResourcePermission([UserRolesEnum.ADMIN]), assignRole)
+
+
+router.route("/refresh-access-token").post(refreshAccessToken)
+
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/logout").post(verifyJWT, logoutUser)
